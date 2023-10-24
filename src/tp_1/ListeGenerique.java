7808709b34dcbe0c10185a7fcab1245fca974a26
MohamedEtteyebEzio <mohamedetteyeb22@gmail.com>
2023-10-24 21:55:36 +0100

    tp1java

---
+++ src/tp_1/ListeGenerique.java
@@ -0,0 +1,9 @@
+package tp_1;
+
+import java.util.ArrayList;
+
+public interface ListeGenerique <T>{
+    abstract  void ajouter(T a, ArrayList<T> ab);
+    abstract  T get( int i,ArrayList<T>tab);
+    abstract  int taille(ArrayList<T>a);
+}
